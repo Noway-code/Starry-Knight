4e71b7d5ccc0283bf1846cd836ecddab52849b11
noway <dragonfury1928@gmail.com>
2023-12-09 18:04:49 -0500

    starChart and moon phases post calls also fixed hardcoded date

--- index.js
+++ index.js
@@ -1,38 +1,88 @@
 const axios = require('axios');
 require('dotenv').config();
- const val = process.env.API_USER;
- const key = process.env.API_SECRET;
+const moment = require("moment");
 
+const val = process.env.API_USER;
+const key = process.env.API_SECRET;
+const date = moment().format("YYYY-MM-DD");
+//Hashing function
 const authString = btoa(`${val}:${key}`);
 
-let data = JSON.stringify({
+// Body Content
+let starData = JSON.stringify({
     "observer": {
-        "date": "2022-02-01",
+        "date": date,
         "latitude": 28.5966398,
         "longitude": -81.2048381
     },
     "style": "navy",
     "view": {
-        "parameters": {
-            "constellation": "ari"
-        },
-        "type": "constellation"
+        "type":"area",
+        "parameters":{
+            "position": {
+                "equatorial": {
+                    "rightAscension": 14.83,
+                    "declination": -15.23
+                }
+            },
+            "zoom": 3
+        }
     }
 });
 
-let config = {
+let moonData = JSON.stringify({
+    "format": "png",
+    "style": {
+        "moonStyle": "default",
+        "backgroundStyle": "stars",
+        "backgroundColor": "red",
+        "headingColor": "white",
+        "textColor": "white"
+    },
+    "observer": {
+        "latitude": 28.5966398,
+        "longitude": -81.2048381,
+        "date": date
+    },
+    "view": {
+        "type": "portrait-simple",
+        "orientation": "south-up"
+    }
+});
+
+// Full request command
+let starChart = {
     method: 'post',
     url: 'https://api.astronomyapi.com/api/v2/studio/star-chart',
     headers: {
         'Content-Type': 'application/json',
         'Authorization': `Basic+ ${authString}`
     },
-    data : data
+    data : starData
 };
 
-axios.request(config)
+let moonPhase = {
+    method: 'post',
+    url: 'https://api.astronomyapi.com/api/v2/studio/moon-phase',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Basic+ ${authString}`
+    },
+    data: moonData
+}
+
+//API Call and logging
+axios.request(starChart)
     .then((response) => {
-        console.log(JSON.stringify(response.data));
+        console.log('starChart: ' +JSON.stringify(response.data));
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+axios.request(moonPhase)
+    .then((response) => {
+        console.log('Moon: ' + JSON.stringify(response.data));
     })
     .catch((error) => {
         console.log(error);
