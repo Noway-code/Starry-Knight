9b3e35c5f3cd8c89e82964fea5c52800500a8708
noway <dragonfury1928@gmail.com>
2023-12-10 20:44:51 -0500

    create event handler for future usage dude discord js is so powerful

--- bot.js
+++ bot.js
@@ -16,6 +16,8 @@ const token = process.env.DISCORD_TOKEN;
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+
+// Command Handler
 client.commands = new Collection();
 
 const foldersPath = path.join(__dirname, 'commands');
@@ -36,32 +38,19 @@ for (const folder of commandFolders) {
     }
 }
 
-client.once(Events.ClientReady, readyClient => {
-    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
+// Connects to event handler
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        return;
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
     }
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-        } else {
-            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-        }
-    }
-});
-// When the client is ready, run this code (only once)
+}
 
 // Log in to Discord with your client's token
 client.login(token);
